3597dbe0dc3e28a5509bb5ac55347d4df754334c
Nurul Amin <NurulAmin7990@gmail.com>
2019-11-06 17:13:35 +0000

    Update

--- GreenLine-Passengers/src/java/ManagedBean/AdminManager.java
+++ GreenLine-Passengers/src/java/ManagedBean/AdminManager.java
@@ -1,26 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ManagedBean;
 
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 
-/**
- *
- * @author Nurul Amin
- */
 @Named(value = "adminManager")
 @SessionScoped
 public class AdminManager implements Serializable {
 
-    /**
-     * Creates a new instance of AdminManager
-     */
     public AdminManager() {
+        
     }
     
 }

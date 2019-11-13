1ef281127b8063fcf88943d78d9eb4649132e7d3
Nurul Amin <NurulAmin7990@gmail.com>
2019-11-13 18:07:50 +0000

    Updated customer register functionality

--- GreenLine-Passengers/src/java/ManagedBeans/CustomerManager.java
+++ GreenLine-Passengers/src/java/ManagedBeans/CustomerManager.java
@@ -1,47 +1,179 @@
 package ManagedBeans;
 
-import dtos.CustomerDTO;
-import java.util.ArrayList;
-import javax.annotation.ManagedBean;
-import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 
-@ManagedBean
-@RequestScoped
-public class CustomerManager {
+//Managed bean scopes
+@Named
+@SessionScoped
+//Class
+public class CustomerManager implements Serializable {
 
-    private ArrayList<CustomerDTO> customers = new ArrayList<>();
-    private CustomerDTO customer;
-    
-    public void fetchCustomerDatabase()
-    {
-        
-    }
-    
-    public void updateCustomerDatabase()
-    {
-        
-    }
-    
-    public void customerDTOUpdate()
-    {
-        customer = new CustomerDTO(0, 0, firstName, surname, dob, passportNumber, 0, 0, gender, country);
-        
-    }
-    
-    public void validateCustomerDetails()
-    {
-        
-    }
+    //Attributes
+    private int personID;
+    private int userID;
+    private String firstName;
+    private String middleName;
+    private String surName;
+    private Date dob;
+    private String passportNumber;
+    private int contactNumber;
+    private int emergencyContactNumber;
+    private String gender;
+    private String country;
+    private String email;
+    private String city;
+    private String password;
 
-    public CustomerDTO getCustomer() {
-        return customer;
+//Getters and setters
+    public String getMiddleName() {
+        return middleName;
     }
 
-    public void setCustomer(CustomerDTO customer) {
-        this.customer = customer;
+    public String getEmail() {
+        return email;
     }
-    
-    
-    
-    
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getPersonID() {
+        return personID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public int getContactNumber() {
+        return contactNumber;
+    }
+
+    public int getEmergencyContactNumber() {
+        return emergencyContactNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPersonID(int personID) {
+        this.personID = personID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    public void setContactNumber(int contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public void setEmergencyContactNumber(int emergencyContactNumber) {
+        this.emergencyContactNumber = emergencyContactNumber;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+//Methodss
+    public String addCustomerToDB() {
+
+        try {
+            DriverManager.registerDriver(
+                    new org.apache.derby.jdbc.ClientDriver());
+            Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/Database", "admin1", "admin1");
+
+            PreparedStatement stmt = con.prepareStatement("INSERT INTO PERSON (FIRSTNAME, MIDDLENAME, SURNAME, DATEOFBIRTH, PASSPORTNUMBER, EMERGANCYCONTACT, GENDER, COUNTRY, EMAIL, CITY, TELEPHONE ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            stmt.setString(1, firstName);
+            stmt.setString(3, surName);
+            stmt.setDate(4, (java.sql.Date) dob);
+            stmt.setString(5, passportNumber);
+            stmt.setInt(6, emergencyContactNumber);
+            stmt.setString(7, gender);
+            stmt.setString(8, country);
+            stmt.setString(2, middleName);
+            stmt.setString(9, email);
+            stmt.setString(10, city);
+            stmt.setInt(11, contactNumber);
+            
+             stmt.execute();
+       
+            stmt.close();
+            con.close();
+            
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        return "index";
+    }
 }
